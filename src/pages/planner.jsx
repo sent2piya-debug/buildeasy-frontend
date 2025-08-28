eab58507de8b09d01097647177987c61ac6d0456
sent2piya-debug <sent2piya@gmail.com>
2025-08-28 19:26:42 +0700

    Create planner.jsx

---
+++ src/pages/planner.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import KitchenPlanner from "../planner/KitchenPlanner";
+
+export default function Planner() {
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <KitchenPlanner />
+    </div>
+  );
+}
