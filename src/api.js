54abde29ee6e3f345c9e21943ac4f0eb1f7202e7
sent2piya-debug <sent2piya@gmail.com>
2025-08-29 20:56:00 +0700

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,1 +1,13 @@
+import axios from "axios";
+
+const API_BASE = import.meta.env.VITE_API_BASE; // will come from Vercel environment
+const api = axios.create({ baseURL: API_BASE });
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+export default api;
 
