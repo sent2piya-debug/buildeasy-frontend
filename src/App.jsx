4f49b97e3ce55293cb37573a7d406a49100b32dd
sent2piya-debug <sent2piya@gmail.com>
2025-08-29 11:11:37 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,31 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate, Outlet } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
-import NavBar from "./components/NavBar";
-import JobsList from "./pages/JobsList";
-import JobDetail from "./pages/JobDetail";
-import PostJob from "./pages/PostJob";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-
-import Planner from "./pages/planner";  // wrapper that loads KitchenPlanner
-
-function Layout() {
-  return (
-    <div>
-      <NavBar />
-      <main style={{ maxWidth: 960, margin: "20px auto", padding: 12 }}>
-        <Outlet />  {/* 👈 this is where page content renders */}
-      </main>
-    </div>
-  );
-}
+import NavBar from "./components/NavBar.jsx";
+import JobsList from "./pages/JobsList.jsx";
+import JobDetail from "./pages/JobDetail.jsx";
+import PostJob from "./pages/PostJob.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+import Planner from "./pages/planner.jsx"; // wrapper page
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<Layout />}>
+    <div>
+      <NavBar />
+      <main style={{ maxWidth: 960, margin: "20px auto", padding: "0 16px" }}>
+        <Routes>
           <Route path="/" element={<Navigate to="/jobs" replace />} />
           <Route path="/jobs" element={<JobsList />} />
           <Route path="/jobs/:id" element={<JobDetail />} />
           <Route path="/post-job" element={<PostJob />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/planner" element={<Planner />} />   {/* 👈 here */}
-        </Route>
-      </Routes>
-    </BrowserRouter>
+          <Route path="/planner" element={<Planner />} />
+          <Route path="*" element={<div>Not Found</div>} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
